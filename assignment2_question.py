349d07a7644d267c71901b43f95aa6a5197e7412
[kunalroutray] <[kunalroutray91221@gmail.com]>
2024-10-15 15:31:58 +0530

    assignment 2 done

--- assignment2_question.py
+++ assignment2_question.py
@@ -1,1 +1,17 @@
-print("                                       ASSIGNMENT2                                                              ")
+#                                                                               ASSIGNMENT2  
+#  Q1__write a python program to input the height of the person and check the height of person is greter than or equal to 6 feet then print message"the person is tall"
+#  Q2__write a python program to enter a mark of a student  and check the mark is greater than 40 or not if yes then pass or fail
+#  Q3__ input an integer through keyboard find out it is odd integer or even iteger  
+#  Q4__ take an input from keyboard write a python program to determine whether the input is capital letter or small letter or a digit or a special character
+#  Q5__           root=-b+-rootof b2-4ac
+#                       ________________
+#                              2a
+#        Write a python program that prompts the user to enter a,b,c and display the result based on descrimenant (if discriminant is +ve equation has 2 roots,
+#        it it is 0 the equation has 1 root , if it is -ve it  has no real roots) else the equation has no roots
+#  Q6__ax+by=e,cx+dy =f, x=ed-bf/ad-bc,y=af-ec/ad-bc
+#      write a python program that prompts the user to enter a,b,c,d,e,f and display the result x and y.if ad-bc is 0 the equation has no solution
+#  Q7__write a program to take x and y coordinate input and find the axix in which the point lies in
+#  Q8__write a pythone program  to find elder among rahul ayush and ajay if their age is entered from user
+#  Q9__write a pythone program that randomly generate an integer bw 1 and 12 and display english month according to that
+#  Q10_write a programe to prompt the user to enter todays day of week also ask the user to enter number of days after todays daay and display the future day of the week
+#  Q11_                                         
